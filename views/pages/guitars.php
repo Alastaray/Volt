f7f2166cec8e61ab0a71d9740f55ddb62fca8370
Alastaray <rikinikto@gmail.com>
2021-12-13 08:20:02 +0200

    Add the pointers on next and previous page.

--- views/pages/guitars.php
+++ views/pages/guitars.php
@@ -1,13 +1,11 @@
-<link rel="stylesheet" href="css/product.css">
 
-<div class="adminprod">
 <?php
     $rowscount=0;
     $c=$this->db->queryOne("SELECT COUNT(*) as 'count' FROM `guitars`");
     if($c)$rowscount=$c["count"];
-    $page=4;
+    $page=1;
     if(isset($_REQUEST["page"])&&is_numeric($_REQUEST["page"])&&$_REQUEST["page"]>=1)$page=$_REQUEST["page"];
-    $limit=3;
+    $limit=5;
     $pag = new Pagination([       
         "rows"=>$rowscount,
         "limit"=>$limit,
@@ -16,20 +14,37 @@
     ]);
     $sql="SELECT * FROM guitars ";
     $sql.= "LIMIT ".$pag->GetFirstRow().", ". $pag->GetLimit();
-    $rows = ($this->db)->queryRows($sql);   
+    $rows = ($this->db)->queryRows($sql); 
+    ?>
+
+    <link rel="stylesheet" href="css/product.css">
+    <div class="guitars">
+    <a class="pointer"><img src='/images/page/pointer_left.png'></a>
+        <div class="adminprod">
+            <?php
     foreach($rows as $row)
      {
         ?>
-        <div class="product">
-        <div class="card">
-            <div class="price"><div><?echo($row["price"]." UAH")?></div></div>
-            <button id="more">Подробнее</button>
-            <img class="img" src=<?echo($row["path"])?>>
-        </div>       
-        <div class="name"><div><?echo($row["name"])?></div></div>
-    </div>
-    <?php
+            <div class="product">
+                <div class="card">
+                    <div class="price">
+                        <div>
+                            <?echo($row["price"]." UAH")?>
+                        </div>
+                    </div>
+                    <button id="more">Подробнее</button>
+                    <img class="img" src=<?echo($row["path"])?>>
+                </div>
+                <div class="name">
+                    <div>
+                        <?echo($row["name"])?>
+                    </div>
+                </div>
+            </div>
+            <?php
      }
     ?>
 
-</div>
+        </div>
+        <a class="pointer"><img src='/images/page/pointer_right.png'></a>
+    </div>
